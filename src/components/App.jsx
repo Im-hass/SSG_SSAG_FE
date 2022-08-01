4cb7deb7cf83a0bda3aa8a71537bd00bb3b60a42
임하스 <huiseon4107@gmail.com>
2022-08-01 16:55:46 +0900

    Update : 컴포넌트 표현식 -> 선언식으로 변경

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { MainPage, NotFoundPage } from '../pages';
 import { Header, Footer } from './common';
+import { ToolbarList } from './toolbar/ToolbarList';
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route exact path="/" element={<MainPage />} />
         <Route element={<NotFoundPage />} />
       </Routes>
+
+      <ToolbarList />
 
       <Footer />
     </div>
--- src/components/toolbar/ToolbarList/ToolbarList.jsx
+++ src/components/toolbar/ToolbarList/ToolbarList.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { ToolbarItem } from '../ToolbarItem';
 import './ToolbarList.scss';
 
-const ToolbarList = () => (
-  <div>
-    <h2>Toolbar List</h2>
-    <div id="m_toolbar" className="mcom_toolbar_v2 react-area">
-      <ul className="toolbar_menu" role="navigation">
-        <ToolbarItem to="/" iconName="icon_category" txt="카테고리" />
-        <ToolbarItem to="/" iconName="icon_search" txt="통합검색" />
-        <ToolbarItem to="/" iconName="icon_house" txt="홈" />
-        <ToolbarItem to="/" iconName="icon_person" txt="My" />
-        <ToolbarItem to="/" iconName="icon_eye" txt="최근본" />
-      </ul>
+function ToolbarList() {
+  return (
+    <div>
+      <h2>Toolbar List</h2>
+      <div id="m_toolbar" className="mcom_toolbar_v2 react-area">
+        <ul className="toolbar_menu" role="navigation">
+          <ToolbarItem to="/" iconName="icon_category" txt="카테고리" />
+          <ToolbarItem to="/" iconName="icon_search" txt="통합검색" />
+          <ToolbarItem to="/" iconName="icon_house" txt="홈" />
+          <ToolbarItem to="/" iconName="icon_person" txt="My" />
+          <ToolbarItem to="/" iconName="icon_eye" txt="최근본" />
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default ToolbarList;
