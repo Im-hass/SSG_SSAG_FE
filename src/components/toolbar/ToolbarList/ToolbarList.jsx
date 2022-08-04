9f1b69c64c147dfdf0ec2ab66b59dc9ee55b5262
Im-hass <huiseon4107@gmail.com>
2022-08-04 15:35:53 +0900

    Fix : 링크 url 변경

--- src/components/toolbar/ToolbarList/ToolbarList.jsx
+++ src/components/toolbar/ToolbarList/ToolbarList.jsx
@@ -24,7 +24,7 @@ const TOOL_LIST = [
     txt: 'My',
   },
   {
-    link: '/history',
+    link: '/historyList',
     iconName: 'icon_eye',
     txt: '최근본',
     imgUrl: 'https://sitem.ssgcdn.com/56/43/16/item/1000034164356_i1_120.jpg',
