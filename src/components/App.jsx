14e6402d273b4166d270fab1d5b92b3df4c675eb
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-08-04 16:00:04 +0900

    Update: 최근 본 상품 페이지 Route 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { MainPage, NotFoundPage } from '../pages';
+import { MainPage, NotFoundPage, RecentShoppingPage } from '../pages';
 import { Header, Footer } from './common';
 import { ToolbarList } from './toolbar/ToolbarList';
 
@@ -16,7 +16,7 @@ function App() {
         <Route path="/search" element={<h1>검색</h1>} />
         <Route path="/my" element={<h1>마이 페이지</h1>} />
         <Route path="/login" element={<h1>로그인</h1>} />
-        <Route path="/historyList" element={<h1>최근 본 목록</h1>} />
+        <Route path="/historyList" element={<RecentShoppingPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
 
