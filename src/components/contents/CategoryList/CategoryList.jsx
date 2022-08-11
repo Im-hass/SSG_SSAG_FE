ec0ff8486a4ecf75f296464fbcc46935ff59214a
Im-hass <huiseon4107@gmail.com>
2022-08-11 18:33:30 +0900

    Temp : 카테고리 임시

--- src/components/contents/CategoryList/CategoryList.jsx
+++ src/components/contents/CategoryList/CategoryList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CategoryItem from '../CategoryItem/CategoryItem';
 import { SubCategoryList } from '../SubCategoryList';
@@ -6,29 +6,43 @@ import { SubCategoryList } from '../SubCategoryList';
 function CategoryList({ data }) {
   const { largeCategoryId, imgUrl, title, mediumCategoryList } = data;
   // const isSelected = new Array(11).fill(false);
-  const [isSelected, setIsSelected] = useState(Array(11).fill(false));
   // const [selectedName, setSelectedName] = useState('');
   // console.log(`is: ${isSelected}`);
+  const [isSelected, setIsSelected] = useState(Array(11).fill(false));
+
+  useEffect(() => {
+    console.log(isSelected);
+  }, [isSelected]);
 
   const handleClick = (e) => {
     e.preventDefault();
     const idx = e.target.dataset.id;
-    console.log();
+    // console.log();
     // const newIsSelected = [...isSelected];
     // newIsSelected.map(() => false);
-    const newIsSelected = new Array(11).fill(false);
-    newIsSelected[idx] = true;
-    console.log('newIsSelected');
-    console.log(newIsSelected);
-    setIsSelected([newIsSelected]);
-    console.log('isSelected');
-    console.log(isSelected);
+    // const newIsSelected = new Array(11).fill(false);
+    // newIsSelected[idx] = true;
+    // console.log('newIsSelected');
+    // console.log(newIsSelected);
+    // setIsSelected([newIsSelected]);
+    // console.log('isSelected');
+    // console.log(isSelected);
+
+    const copyArray = [...isSelected]; // ES6 deep copy
+    copyArray[idx] = true;
+    setIsSelected(copyArray);
   };
 
   return (
     <li className="clnb_renew_item selected">
       {/* li 클릭 시 selected 추가 */}
-      <a href="/" className="clickable" onClick={handleClick}>
+      <a
+        href="/"
+        className={
+          isSelected[largeCategoryId - 1] ? 'clickable selected' : 'clickable'
+        }
+        onClick={handleClick}
+      >
         <span
           data-id={largeCategoryId - 1}
           className="thmb ssg_lazy"
@@ -40,6 +54,7 @@ function CategoryList({ data }) {
       </a>
 
       <SubCategoryList
+        isSelected={isSelected}
         largeCategoryId={largeCategoryId}
         mediumCategoryList={mediumCategoryList}
       />
