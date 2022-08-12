bd6aa59b2bd6823172b239ab66efc1840234a881
Im-hass <huiseon4107@gmail.com>
2022-08-12 09:21:56 +0900

    Fix : index에서 scss import  제거

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Root from './Root';
-import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
