de0be7c2a666bd85c16f18b4261902c417612254
Im-hass <huiseon4107@gmail.com>
2022-08-12 09:35:08 +0900

    Update : 데이터 분리

---
+++ src/assets/datas/ServiceList.js
@@ -0,0 +1,56 @@
+export const SERVICE_LIST_DATA = [
+  {
+    to: 'https://m-shinsegaemall.ssg.com/page/happybuy.ssg',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202106/2021063015414495676999174799_796.png&w=150&h=150&edit=c&t=41d0e0fc92b622800080308be058134b661bcf29',
+    title: '해피라운지',
+  },
+  {
+    to: 'https://m.ssg.com/page/giftsub.ssg?mGnbYn=N&mccode=6005',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202106/2021063015420343699151226025_107.png&w=150&h=150&edit=c&t=a4f90bfd374c4c1260953082c04ed2d9559c069b',
+    title: '선물하기',
+  },
+  {
+    to: 'https://m-shinsegaemall.ssg.com/special/index.ssg',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202106/2021063015422256402610888261_65.png&w=150&h=150&edit=c&t=50091b21722893b184dc901fd37e62b496e0e7d0',
+    title: '공식브랜드관',
+  },
+  {
+    to: 'https://m-shinsegaemall.ssg.com/page/ssgdept.ssg',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202206/2022062117425937341839397183_525.png&w=150&h=150&edit=c&t=23310b33bc2774f9da90f014e0e1c475f5a69786',
+    title: '백화점',
+  },
+  {
+    to: 'https://m-shinsegaemall.ssg.com/event/eventMain.ssg',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202208/2022080211035212256128788612_829.png&w=150&h=150&edit=c&t=f626f7682b2e7b6b3e8a0ea7f06f4197cd1fc02d',
+    title: '이벤트',
+  },
+  {
+    to: 'https://m-shinsegaemall.ssg.com/plan/cu/shoppingPlanShopShmall.ssg',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202106/2021063015425942531544410254_839.png&w=150&h=150&edit=c&t=7ee3f94c2ad8f2d6183d69cfcf579994e36b9c35',
+    title: '기획전',
+  },
+  {
+    to: 'https://m-shinsegaemall.ssg.com/page/ranking.ssg',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202106/2021063015430898413736468373_392.png&w=150&h=150&edit=c&t=bbdff5c77f147265c5ef51a43fd8ed2264b3fe50',
+    title: '랭킹',
+  },
+  {
+    to: 'https://m-shinsegaemall.ssg.com/page/newarrivals.ssg',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202106/2021063015431965926494480749_140.png&w=150&h=150&edit=c&t=eb673a6d7d254a7f7a3038c6e2a90ced5708a638',
+    title: '신상품',
+  },
+  {
+    to: 'https://m-shinsegaemall.ssg.com/contents/lifeMagazineMain.ssg',
+    bgImgUrl:
+      'https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202106/2021063015433874539700721080_714.png&w=150&h=150&edit=c&t=bdab871e1dec4644a230903dc3a44f31cecfe2ef',
+    title: '매거진',
+  },
+];
