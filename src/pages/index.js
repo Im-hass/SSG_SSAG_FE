75dd1f1f0779b6ec4357674321ed6b6caae2b68f
Im-hass <huiseon4107@gmail.com>
2022-08-25 17:59:11 +0900

    Feat: 상품 목록 페이지 카테고리 목록 구현

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,3 +11,4 @@ export { default as MyPage } from './MyPage';
 export { default as ProductDetailPage } from './ProductDetailPage';
 export { default as SignUpPage } from './SignUp/SignUpPage';
 export { default as LoginPage } from './LoginPage';
+export { default as ProductListPage } from './ProductListPage';
