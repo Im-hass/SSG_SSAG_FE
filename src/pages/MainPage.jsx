74261628bbf79949dd6cba5333837b001ba38505
Im-Hass <huiseon4107@gmail.com>
2022-08-26 00:27:05 +0900

    Style: 메인 페이지 헤더 sticky, SearchBox min-height 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
 import { ToolbarList } from '../components/toolbar/ToolbarList';
-import { Header, Footer } from '../components/common/index';
+import { Header, Footer } from '../components/common';
 import Home from './Home/Home';
+import { isOpenState } from '../recoil/states';
 
 function MainPage() {
+  const [isOpen] = useRecoilState(isOpenState);
   return (
-    <div>
+    <div id="m_wrap" className="mcom_wrap sm_v3 sm_page_main has_smhero_banner">
       <Header />
-      <Home />
-      <ToolbarList />
-      <Footer pageName="main" />
+      {!isOpen && (
+        <>
+          <Home />
+          <ToolbarList />
+          <Footer pageName="main" />
+        </>
+      )}
     </div>
   );
 }
