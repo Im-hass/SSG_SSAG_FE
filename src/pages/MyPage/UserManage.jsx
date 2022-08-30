a6e43fb11d0c56c2a830185a74cd3d79b264d29a
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-08-30 14:11:14 +0900

    Fix : 배송지 관리 Link 추가

--- src/pages/MyPage/UserManage.jsx
+++ src/pages/MyPage/UserManage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './UserManage.scss';
 
 function UserManage() {
@@ -7,9 +8,9 @@ function UserManage() {
       <span>
         <span className="myssg_manage_text ty_like">좋아요</span>
       </span>
-      <span>
+      <Link to="/destination">
         <span className="myssg_manage_text ty_delivery">배송지 관리</span>
-      </span>
+      </Link>
       <span>
         <span className="myssg_manage_text ty_alert">알림함</span>
       </span>
