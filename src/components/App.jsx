9070ec606033b838eb82fbe96cd335c76342089b
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-08-31 14:12:15 +0900

    Fix : MyPage import 수정

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import {
   CartPage,
   DestinationPage,
   MainPage,
-  MyPage,
   NotFoundPage,
   RecentShoppingPage,
   SearchResultPage,
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ export { default as AddDestinationPage } from './AddDestinationPage';
 export { default as CartPage } from './CartPage';
 export { default as DestinationPage } from './DestinationPage';
 export { default as MainPage } from './MainPage';
-export { default as MyPage } from './MyPage';
 export { default as NotFoundPage } from './NotFoundPage';
 export { default as PaymentMeansPage } from './PaymentMeansPage';
 export { default as RecentShoppingPage } from './RecentShoppingPage';
