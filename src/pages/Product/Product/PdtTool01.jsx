b966db36d6a23cbc25cfaa165ddee53bc0567db3
Im-hass <huiseon4107@gmail.com>
2022-09-07 13:54:36 +0900

    Update: 상품 상세 페이지 좋아요 기능 추가, 결제수단 관리 에러 메시지 추가

--- src/pages/Product/Product/PdtTool01.jsx
+++ src/pages/Product/Product/PdtTool01.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
+import { LikeButton } from '../../../components/ui/LikeButton';
 import './style/PdtTool01.scss';
 
-function PdtTool01({ buyBtn, handleOpenBtn }) {
+function PdtTool01({
+  buyBtn,
+  productData,
+  handleOpenBtn,
+  isWishChange,
+  setIsWishChange,
+}) {
   return (
     <div className={`btm_bgn_in dps1 ${buyBtn}`}>
       <ul className="btm_bgn_bx type_other1">
         <li className="ty_like">
-          <span className="cmlike _js_cmlike interestIt clickable">
-            <input type="hidden" name="attnDivCd" value="10" />
-            <input type="hidden" name="siteNo" value="6004" />
-            <input type="hidden" name="attnTgtIdnfNo1" value="1000310147566" />
-            <input type="hidden" name="attnTgtIdnfNo2" value="6005" />
-            <input type="hidden" name="notiTitle" value="론디 01(BR)" />
-            <input
-              type="hidden"
-              name="notiImgPath"
-              value="https://sitem.ssgcdn.com/66/75/14/item/1000310147566_i1_70.jpg"
-            />
-            <input type="hidden" name="sellStatCd" value="20" />
-            <input type="hidden" name="soldoutYn" value="N" />
-            <input type="hidden" name="checked" value="N" />
-            <input type="hidden" name="uitemIdAndSalestrNo" value="00000" />
-
-            <button
-              type="button"
-              className="cmlike_btn _js_cmlike_btn enp_mobon_wish"
-            >
-              <span className="cmlike_ico">
-                <i className="cmlike_primary_l" />
-                <span className="sr_off">
-                  <span className="blind">관심상품 취소</span>
-                </span>
-                <span className="sr_on">
-                  <span className="blind">관심상품 등록</span>
-                </span>
-              </span>
-            </button>
-          </span>
+          <LikeButton
+            wishDto={productData.wishDto}
+            productId={productData.productId}
+            isWishChange={isWishChange}
+            setIsWishChange={setIsWishChange}
+          />
         </li>
         <li>
           <a
